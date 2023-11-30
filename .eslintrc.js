e17291d080aa740a4182602e8fc862888ffd4a86
FahadDev2 <fahadss@zohomail.com>
2023-11-30 21:03:32 +0300

    eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    semi: ['error', 'never'],
+    'prettier/prettier': 2,
+    quotes: ['error', 'single'],
+    'no-console': 1,
+    'no-var': 'error',
+    'prefer-const': 'error',
+  },
+};
